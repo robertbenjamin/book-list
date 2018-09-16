eada5c6bb6f5ee4f6d40ff99b341d8c1d18975ac
Robert Benjamin <rotberj@gmail.com>
2018-09-15 20:24:45 -0700

    Make BookList component functional

--- client/src/components/BookList/BookList.js
+++ client/src/components/BookList/BookList.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class BookList extends Component {
-  removeBook = id => {
-    this.props.onRemove(id);
-  }
+const BookList = props => {
+  const bookList = props.books.map(book =>
+    <li key={book._id}>
+      <span>{book.title}, {book.author}</span>&nbsp;
+        <button onClick={() => props.onRemove(book._id)}>Remove</button>
+    </li>
+  );
 
-  render() {
-    const bookList = this.props.books.map(book =>
-      <li key={book._id}>
-        <span>{book.title}, {book.author}</span>&nbsp;
-        <button onClick={() => this.removeBook(book._id)}>Remove</button>
-      </li>
-    );
-
-    return (
-      <section className='book-list'>
-        <h2>Book List</h2>
-        <ul>{bookList}</ul>
-      </section>
-    );
-  }
+  return (
+    <section className='book-list'>
+      <h2>Book List</h2>
+      <ul>{bookList}</ul>
+    </section>
+  );
 }
 
 export default BookList;
